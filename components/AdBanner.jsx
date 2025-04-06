f7d24b4516126641b028cfb1e98c18cc9793e369
Naved <naved10055@gmail.com>
2025-04-06 22:33:46 +0530

    checking adsenc

--- components/AdBanner.jsx
+++ components/AdBanner.jsx
@@ -26,22 +26,6 @@ const AdBanner = ({
       data-ad-format={dataAdFormat}
       data-full-width-responsive={dataFullWidthResponsive.toString()}
     ></ins>
-    // <amp-ad width="100vw" height="320"
-    //   type="adsense"
-    //   data-ad-client="ca-pub-5765353401158683"
-    //   data-ad-slot="8376755834"
-    //   data-auto-format="mcrspv"
-    //   data-full-width="">
-    //   <div overflow=""></div>
-    // </amp-ad>
-//     <amp-ad width="100vw" height="320"
-//      type="adsense"
-//      data-ad-client="ca-pub-5765353401158683"
-//      data-ad-slot="2394571922"
-//      data-auto-format="rspv"
-//      data-full-width="">
-//   <div overflow=""></div>
-// </amp-ad>
   );
 };
 
