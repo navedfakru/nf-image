a4b471537f419c38f4bd195e133af137b48f6ba4
Naved <naved10055@gmail.com>
2025-04-11 22:03:57 +0530

    btn

--- components/PhoneApp.jsx
+++ components/PhoneApp.jsx
@@ -1,10 +1,13 @@
 "use client"
 
+import Link from 'next/link'
 import React from 'react'
 
 function PhoneApp() {
   return (
-    <div>PhoneApp</div>
+    <div className='w-full h-full flex items-center justify-center bg-red-400/20'>
+      <Link href={'/bg-remover'} className='px-4 py-2 bg-blue-500 text-white'>Go to bg remover</Link>
+    </div>
   )
 }
 
